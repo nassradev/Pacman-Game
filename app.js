4dbb2d06eb86da5b6a0a8d38484b10887fe7df3c
nassra <nassra96@gmail.com>
2020-11-17 20:05:24 +0000

    compressed

--- app.js
+++ app.js
@@ -1,161 +1,169 @@
-    const width = 28
-    const grid = document.querySelector('.grid')
-    const score = document.querySelector('#score')
-    let squares = []
+        const width = 28
+        const grid = document.querySelector('.grid')
+        let score = document.querySelector('#score')
+        let pcScore = 0
+        let squares = []
 
-    //28 * 28 = 784
-    // 0 - food
-    // 1 - wall
-    // 2 - ghost-lair
-    // 3 - super food
-    // 4 - empty
+        //28 * 28 = 784
+        // 0 - food
+        // 1 - wall
+        // 2 - ghost-lair
+        // 3 - super food
+        // 4 - empty
 
 
-    const layout = [
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-        4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
-        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
-        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+        const layout = [
+            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+            1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+            1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+            1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+            4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+            1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+            1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+            1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+            1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+            1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+            1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+            1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+            1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+            1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
 
-    ]
+        ]
 
-    const createBoard = function (){
-        for (let i = 0; i < layout.length; i++) {
-            const square = document.createElement('div')
-            grid.appendChild(square)
-            squares.push(square)
+        const createBoard = function (){
+            for (let i = 0; i < layout.length; i++) {
+                const square = document.createElement('div')
+                grid.appendChild(square)
+                squares.push(square)
 
-            if (layout[i] === 0){
-    squares[i].classList.add('food')
-            }  else if(layout[i] === 1){
-    squares[i].classList.add('wall')
-            } else if(layout[i] === 2){
-    squares[i].classList.add('ghost-lair')
-            } else if(layout[i] === 3){
-    squares[i].classList.add('power-up')
-            } else {
-                squares[i].classList.add('empty')
+                if (layout[i] === 0){
+        squares[i].classList.add('food')
+                }  else if(layout[i] === 1){
+        squares[i].classList.add('wall')
+                } else if(layout[i] === 2){
+        squares[i].classList.add('ghost-lair')
+                } else if(layout[i] === 3){
+        squares[i].classList.add('power-up')
+                } else {
+                    squares[i].classList.add('empty')
+                }
             }
+
+
         }
 
+        createBoard()
 
-    }
-
-    createBoard()
-
-    //pacman starts at beginning 
-    let pacmanStart = 391
-    squares[pacmanStart].classList.add('pacman')
-    //if the space is equal to 1 he cant go anywhere 
-    //if the space next to him is = 0 or 4 pacman can move 
-
-    //Key up
-    document.addEventListener('keyup', function (userKey){
-    if (userKey.key === 'ArrowUp')
-    {
-            if (pacmanStart - width >=0) {
-            squares[pacmanStart].classList.remove('pacman')  
-            pacmanStart -= width
-            squares[pacmanStart].classList.add('pacman')   
-                if (squares[pacmanStart].classList.contains('food')){
-                console.log('hello') 
+//Eat dots function 
+let eatDots = function (){
+    if (squares[pacmanStart].classList.contains('food')){      
+            pcScore++
+            score.innerHTML = `Score: ${pcScore}` 
+            console.log(`Score: ${pcScore}`)
             squares[pacmanStart].classList.remove('food')
             squares[pacmanStart].classList.add('pacman')   
-                }
-                 if (squares[pacmanStart].classList.contains('power-up')){
-                            console.log('hello') 
-                        squares[pacmanStart].classList.remove('power-up')
-               
-                        squares[pacmanStart].classList.add('pacman')  
-                       
-                         }
- 
-            }}   
-         
-          
-    })
+    }}
+//pacman starts at beginning 
+let pacmanStart = 391
+squares[pacmanStart].classList.add('pacman')
 
-    //Key down 
-    document.addEventListener('keyup', function (userKey){
-    if (userKey.key === 'ArrowDown')
-    {
-        if (pacmanStart + width < width * width){
-           
-            squares[pacmanStart].classList.remove('pacman')
-            pacmanStart += width
-            squares[pacmanStart].classList.add('pacman')
-                //If it is a food pacman will replace the div (and eat the food)
-              if (squares[pacmanStart].classList.contains('food')){
-                console.log('hello') 
-            squares[pacmanStart].classList.remove('food')
-            squares[pacmanStart].classList.add('pacman')   
-                } else 
-                //If it is a power up pacman will replace thediv
-                //change to power up
+//Power up function 
+let powerUp = function (){
                 if (squares[pacmanStart].classList.contains('power-up')){
-                            console.log('hello') 
-                        squares[pacmanStart].classList.remove('power-up')
+                    squares[pacmanStart].classList.remove('power-up')
+                    squares[pacmanStart].classList.add('pacman')  
+                    pcScore += 5
+                    score.innerHTML = `Score: ${pcScore}` 
+                    console.log(`Score: ${pcScore}`)  
 
-                        squares[pacmanStart].classList.add('pacman')  
-                       
-                         } 
+            }}
+//Out of bound left
+let outOfBoundsLeft = function (){
+         if (pacmanStart === 364) {
+                    pacmanStart = 391
+                    squares[pacmanStart].classList.remove('pacman') 
+                }
+}
 
+//Out of bounds right 
+let outOfBoundsRight = function(){
+         if (pacmanStart === 391) {
+                    pacmanStart = 364
+                    squares[pacmanStart].classList.remove('pacman') 
+                }
+}
+
+
+    
+//Key up
+document.addEventListener('keyup', function (userKey){
+if (userKey.key === 'ArrowUp')
+        {
+        if (pacmanStart - width >=0 && !squares[pacmanStart - width].classList.contains('wall')) {
+             squares[pacmanStart].classList.remove('pacman')  
+                pacmanStart -= width
+                squares[pacmanStart].classList.add('pacman')   
+                eatDots()
+                powerUp()
+        }}
+        })
+
+//Key down 
+document.addEventListener('keyup', function (userKey){
+        if (userKey.key === 'ArrowDown')
+        {
+            if (pacmanStart + width < width * width && !squares[pacmanStart + width].classList.contains('wall')){
+            if (!squares[pacmanStart + width].classList.contains('ghost-lair')){
+                squares[pacmanStart].classList.remove('pacman')
+                pacmanStart += width
+                squares[pacmanStart].classList.add('pacman')
+                eatDots()
+                powerUp()   
+                    
+                }} 
+            }
                 
-            }  }   
+    })
+        
+//Key left
+document.addEventListener('keyup', function (userKey){
+    if (userKey.key === 'ArrowLeft')
+    {
+        if (pacmanStart % width !== 0 && !squares[pacmanStart-1].classList.contains('wall')){   
+            squares[pacmanStart].classList.remove('pacman')    
+            pacmanStart -=1
+            outOfBoundsLeft()
+            squares[pacmanStart].classList.add('pacman')
+            eatDots()
+            powerUp()
+         }
+    }
     })
 
-    //Key left
-    document.addEventListener('keyup', function (userKey){
-        if (userKey.key === 'ArrowLeft'){
-    if (pacmanStart % width !== 0){
-    squares[pacmanStart].classList.remove('pacman')    
-    pacmanStart -=1
-        squares[pacmanStart].classList.add('pacman')
-          if (squares[pacmanStart].classList.contains('food')){
-                console.log('hello') 
-            squares[pacmanStart].classList.remove('food')
-            squares[pacmanStart].classList.add('pacman')   
-                }
-    }}
-    })
 
-
-    //Key right
-    document.addEventListener('keyup', function (userKey){
+//Key right
+document.addEventListener('keyup', function (userKey){
     if (userKey.key === 'ArrowRight'){
-        console.log('key right pressed')
-        if (pacmanStart  % width < width -1){
-    squares[pacmanStart].classList.remove('pacman')
-    pacmanStart +=1
-        squares[pacmanStart].classList.add('pacman')
-          if (squares[pacmanStart].classList.contains('food')){
-                console.log('hello') 
-            squares[pacmanStart].classList.remove('food')
-            squares[pacmanStart].classList.add('pacman')   
-                }
-    }}
+        if (pacmanStart  % width < width -1 && !squares[pacmanStart+1].classList.contains('wall')){            squares[pacmanStart].classList.remove('pacman')
+            pacmanStart +=1
+            outOfBoundsRight()
+            squares[pacmanStart].classList.add('pacman')
+            eatDots()
+            powerUp()
+        }}
     })
+
