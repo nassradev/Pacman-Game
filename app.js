f7071222652e20e8ae0acff07a2c3bc8aa89122f
nassra <nassra96@gmail.com>
2020-11-17 16:40:26 +0000

    updated

--- app.js
+++ app.js
@@ -49,9 +49,55 @@ const createBoard = function (){
         grid.appendChild(square)
         squares.push(square)
 
+        if (layout[i] === 0){
+squares[i].classList.add('food')
+        }  else if(layout[i] === 1){
+squares[i].classList.add('wall')
+        } else if(layout[i] === 2){
+squares[i].classList.add('ghost-lair')
+        } else if(layout[i] === 3){
+squares[i].classList.add('power-up')
+        } else {
+            squares[i].classList.add('empty')
+        }
     }
 
 
 }
 
 createBoard()
+
+//pacman starts at beginning 
+let pacmanStart = 391
+squares[pacmanStart].classList.add('pacman')
+//if the space is equal to 1 he cant go anywhere 
+//if the space next to him is = 0 or 4 pacman can move 
+
+//Key up
+document.addEventListener('keyup', function (userKey){
+if (userKey.key === 'ArrowUp'){
+console.log('key up pressed')
+}
+ })
+
+//Key down 
+document.addEventListener('keyup', function (userKey){
+ if (userKey.key === 'ArrowDown'){
+ console.log('key down pressed')   
+    }   
+ })
+
+//Key left
+ document.addEventListener('keyup', function (userKey){
+ if (userKey.key === 'ArrowLeft'){
+  console.log('key left pressed')     
+    } 
+ })
+
+
+//Key right
+ document.addEventListener('keyup', function (userKey){
+if (userKey.key === 'ArrowRight'){
+    console.log('key right pressed')
+    }
+ })
